326c883dafca33c07abc5fad6ef68d9fc966458d
Jamie Moore <17076460@stu.mmu.ac.uk>
2020-03-31 00:26:12 +0100

    Add buttons for accessibility
    Skip animation and page reload buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
+// Animation to appear after care sequence
 const animation = function() {
     const header = document.querySelector('.site-header');
     const banner = document.querySelector('.cars-intro');
-    const animCars = document.querySelector('.animation');
 
     var tl = new TimelineMax({delay: 15});
     tl
@@ -10,3 +10,28 @@ const animation = function() {
 }
 
 animation();
+
+// Skip content
+document.querySelector('.skip-content').addEventListener('click', function(e) {
+    e.preventDefault();
+    const animCars = document.querySelector('.animation');
+    animCars.style.display = "none";
+
+    const animation = function() {
+        const header = document.querySelector('.site-header');
+        const banner = document.querySelector('.cars-intro');
+    
+        var tl = new TimelineMax({});
+        tl
+        .to(header, 1, {opacity: 1})
+        .to(banner, 1, {opacity: 1}, 0);
+    }
+    
+    animation();
+
+});
+
+// Reload page
+document.querySelector('.reload-animation').addEventListener('click', function() {
+    location.reload();
+});
